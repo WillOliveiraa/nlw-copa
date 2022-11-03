e485342de6fe69b598aa97d786c8dbe2a2044e8d
WillOliveiraa <willi.oliveira93@gmail.com>
2022-11-03 07:13:31 -0300

    Add new request in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import { z as zod } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({ log: ['query'] });
 
@@ -15,6 +17,39 @@ async function boostrap() {
         const count = await prisma.pool.count();
 
         return { count };
+    });
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count };
+    });
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = zod.object({
+            title: zod.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generateCode = new ShortUniqueId({ length: 6 });
+
+        const code = String(generateCode()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code,
+            },
+        });
+
+        return reply.status(201).send({ code });
     });
 
     await fastify.listen({ port: 3333 });
